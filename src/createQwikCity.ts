f8fb2201c275297bfb89bce55e38a8dec7f96257
gioboa <giorgiob.boa@gmail.com>
2023-03-20 22:45:38 +0100

    fix createQwikCity

--- src/createQwikCity.ts
+++ src/createQwikCity.ts
@@ -1,114 +1,135 @@
 import type { RenderOptions } from '@builder.io/qwik';
-import { _deserializeData, _serializeData, _verifySerializable } from '@builder.io/qwik';
+import {
+	_deserializeData,
+	_serializeData,
+	_verifySerializable,
+} from '@builder.io/qwik';
 import type {
-    ServerRenderOptions,
-    ServerRequestEvent
+	ServerRenderOptions,
+	ServerRequestEvent,
 } from '@builder.io/qwik-city/middleware/request-handler';
 import {
-    mergeHeadersCookies,
-    requestHandler
+	mergeHeadersCookies,
+	requestHandler,
 } from '@builder.io/qwik-city/middleware/request-handler';
 import type { Render } from '@builder.io/qwik/server';
 import { setServerPlatform } from '@builder.io/qwik/server';
 import qwikCityPlan from '@qwik-city-plan';
-import type { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import type { APIGatewayProxyResult, Context } from 'aws-lambda';
 
 /**
  * @alpha
  */
 export function createQwikCity(opts: QwikCityAzureOptions) {
-    const qwikSerializer = {
-        _deserializeData,
-        _serializeData,
-        _verifySerializable,
-    };
-    if (opts.manifest) {
-        setServerPlatform(opts.manifest);
-    }
-    async function handler(event: APIGatewayEvent, context: Context): Promise<any> {
-        try {
-            const url = new URL(event.path, 'http://localhost:3000');
-            const options: RequestInit = {
-                method: event.httpMethod || 'GET',
-                headers: [],
-                body: event.body,
-            };
+	const qwikSerializer = {
+		_deserializeData,
+		_serializeData,
+		_verifySerializable,
+	};
+	if (opts.manifest) {
+		setServerPlatform(opts.manifest);
+	}
+	async function handler(event: any, context: Context): Promise<any> {
+		console.log('context', JSON.stringify(event), JSON.stringify(context));
 
-            const serverRequestEv: ServerRequestEvent<APIGatewayProxyResult> = {
-                mode: 'server',
-                locale: undefined,
-                url,
-                platform: context,
-                env: {
-                    get(key) {
-                        return process.env[key];
-                    },
-                },
-                // @ts-ignore
-                request: new Request(url, options),
-                getWritableStream: (status, headers, cookies, resolve) => {
-                    let bodyChunk = new Uint8Array();
-                    const response: APIGatewayProxyResult = {
-                        statusCode: status,
-                        body: '',
-                        headers: {},
-                    };
-                    mergeHeadersCookies(headers, cookies).forEach(
-                        (value, key) => (response.headers![key] = value)
-                    );
-                    return new WritableStream({
-                        write(chunk: Uint8Array) {
-                            if (bodyChunk instanceof Uint8Array) {
-                                const newBuffer = new Uint8Array(bodyChunk.length + chunk.length);
-                                newBuffer.set(bodyChunk);
-                                newBuffer.set(chunk, bodyChunk.length);
-                                bodyChunk = newBuffer;
-                            }
-                        },
-                        close() {
-                            response.body = new TextDecoder().decode(bodyChunk);
-                            resolve(response);
-                        },
-                    });
-                },
-            };
+		const request = event.Records[0].cf.request;
+		const fullPath = `https://${
+			event.Records[0].cf.config.distributionDomainName
+		}${request.uri}${request.querystring ? `?${request.querystring}` : ''}`;
+		try {
+			const url = new URL(fullPath);
+			const options: RequestInit = {
+				method: event.httpMethod || 'GET',
+				headers: [],
+				body: event.body,
+			};
 
-            // send request to qwik city request handler
-            const handledResponse = await requestHandler(serverRequestEv, opts, qwikSerializer);
-            if (handledResponse) {
-                handledResponse.completion.then((err) => {
-                    if (err) {
-                        console.error(err);
-                    }
-                });
-                const response = await handledResponse.response;
-                if (response) {
-                    return response;
-                }
-            }
+			const serverRequestEv: ServerRequestEvent<APIGatewayProxyResult> = {
+				mode: 'server',
+				locale: undefined,
+				url,
+				platform: context,
+				env: {
+					get(key) {
+						return process.env[key];
+					},
+				},
+				// @ts-ignore
+				request: new Request(url, options),
+				getWritableStream: (status, headers, cookies, resolve) => {
+					let bodyChunk = new Uint8Array();
+					const response: any = {
+						status,
+						body: '',
+						headers: {},
+					};
+					mergeHeadersCookies(headers, cookies).forEach(
+						(value, key) => (response.headers![key] = value)
+					);
+					response.headers = mapHeadersToAwsHeaders(response.headers);
+					return new WritableStream({
+						write(chunk: Uint8Array) {
+							if (bodyChunk instanceof Uint8Array) {
+								const newBuffer = new Uint8Array(
+									bodyChunk.length + chunk.length
+								);
+								newBuffer.set(bodyChunk);
+								newBuffer.set(chunk, bodyChunk.length);
+								bodyChunk = newBuffer;
+							}
+						},
+						close() {
+							response.body = new TextDecoder().decode(bodyChunk);
+							resolve(response);
+						},
+					});
+				},
+			};
 
-            // TODO: not found
-        } catch (e: any) {
-            console.error(e);
-            return {
-                status: 500,
-                headers: { 'Content-Type': 'text/plain; charset=utf-8' },
-            };
-        }
-    }
+			// send request to qwik city request handler
+			console.log('handledResponse-pre', fullPath);
+			const handledResponse = await requestHandler(
+				serverRequestEv,
+				opts,
+				qwikSerializer
+			);
+			console.log('handledResponse', handledResponse);
+			if (handledResponse) {
+				handledResponse.completion.then((err) => {
+					if (err) {
+						console.error(err);
+					}
+				});
+				const response = await handledResponse.response;
+				console.log('----response----', response);
+				
+				if (response) {
+					return response;
+				}
+			}
 
-    return handler;
+			// TODO: not found
+		} catch (e: any) {
+			console.error(e);
+			return {
+				status: 500,
+				headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+			};
+		}
+	}
+
+	return handler;
 }
 
 /**
  * @alpha
  */
-export interface QwikCityAzureOptions extends ServerRenderOptions { }
+export interface QwikCityAzureOptions extends ServerRenderOptions {}
 
 /**
  * @alpha
  */
-export interface PlatformAzure extends Partial<Context> { }
+export interface PlatformAzure extends Partial<Context> {}
 
 /**
  * @alpha
@@ -125,5 +146,15 @@ export interface PlatformAzure extends Partial<Context> { }
  * ```
  */
 export function qwikCity(render: Render, opts?: RenderOptions) {
-    return createQwikCity({ render, qwikCityPlan, ...opts });
+	return createQwikCity({ render, qwikCityPlan, ...opts });
 }
+
+const mapHeadersToAwsHeaders = (headers = {}) => {
+	const awsHeaders: any = {};
+	console.log('headers', headers);
+	Object.entries(headers).forEach(([key, value]) => {
+		awsHeaders[key] = [{ key, value }];
+	});
+	console.log('awsHeaders', awsHeaders);
+	return awsHeaders;
+};
